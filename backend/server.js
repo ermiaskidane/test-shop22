2d6f555f26e123eb3e5ac22047da535db9f68499
ermias kidane <ermiaskidanegebre@gmail.com>
2022-04-15 02:54:51 +0100

    make it better

--- backend/server.js
+++ backend/server.js
@@ -37,6 +37,17 @@ app.get('/api/config/paypal', (req, res) =>
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.js'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...')
+  })
+}
 // // Custom Error Handler for Routes does not exist
 app.use(notFound)
 
